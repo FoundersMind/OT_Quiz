2589a6c68adb808d2ea98d095b7a56e5625c45ee
Naman Jain <134630688+Sakhtiman@users.noreply.github.com>
2025-07-12 23:48:10 +0530

    changes

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -123,15 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-# settings.py
 
+STATIC_URL = '/static/'
 
-
-# Add this if not already present (optional but useful)
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
