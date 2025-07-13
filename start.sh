a74afeb29fd1337084420895d250f93e936a8d62
Naman Jain <134630688+Sakhtiman@users.noreply.github.com>
2025-07-13 20:13:19 +0530

    changes

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Use default 8000 if $PORT is not set (for local)
+PORT=${PORT:-8000}
+
 # Start Nginx
 service nginx start
 
-# Start Gunicorn on Railway-provided port
+# Start Gunicorn
 exec gunicorn quiz_project.wsgi:application --bind 0.0.0.0:$PORT
