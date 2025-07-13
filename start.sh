bbec7d289f73d720aedccad3e2a258fbf5cda9e5
Naman Jain <134630688+Sakhtiman@users.noreply.github.com>
2025-07-13 22:47:56 +0530

    changes

--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 
 # Start Nginx in background
 service nginx start
+# Apply Django migrations
+python manage.py makemigrations
+python manage.py migrate
 python manage.py shell < create_superuser.py
 # Run Gunicorn on port 8000
 exec gunicorn quiz_project.wsgi:application --bind 0.0.0.0:8000 --workers 3
